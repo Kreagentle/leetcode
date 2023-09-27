b4f99afeed04efd107b42a55a6532627350f2096
Ekaterina Kuzevanova <ekaterina.kuzevanova@quodorbis.com>
2023-09-28 00:16:22 +0100

    Best score task

---
+++ Array/best_score.py
@@ -0,0 +1,15 @@
+def first_second(my_list):
+    max1, max2 = my_list[0], my_list[0]
+
+    for n in my_list:
+        if n > max1:
+            max2 = max1
+            max1 = n
+        elif n > max2:
+            max2 = n
+
+    return max1, max2
+
+if __name__ == '__main__':
+    myList = [84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0]
+    print(first_second(myList))
